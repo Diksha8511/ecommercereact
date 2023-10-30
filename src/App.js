4277bdefcfe14785555b2a6b7e971333def2ed7b
Diksha Pandit <diksha.pandit@krishtechnolabs.com>
2023-10-30 18:51:40 +0530

    Create header and footer component, Set routing,grouping imports

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+//pages
+import { Home, Contact } from './pages'
+//components
+import { Header, Footer } from './components'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
